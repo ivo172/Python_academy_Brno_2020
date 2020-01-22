ca9bb4abdc8210f51944f406c2faed58d566d340
Ivo Marek <ivo172@gmail.com>
2020-01-22 20:41:32 +0100

    OK

---
+++ Create_list_add-elements.py
@@ -0,0 +1,22 @@
+# Create Candidate
+
+
+# Print candidates at the beginning
+
+
+# Create employees
+
+
+# Print employees at the beginning
+
+
+# Add new candidates
+
+
+# Print new candidates
+
+
+# Insert name
+
+
+# Print the employees list after entering a new name
