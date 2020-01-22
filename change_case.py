6d389b902663b1c18f2e674bbbf012e76624d1cd
Ivo Marek <ivo172@gmail.com>
2020-01-22 19:46:20 +0100

    OK

--- change_case.py
+++ change_case.py
@@ -1,1 +1,12 @@
+"""
+Create a python script named change_case.py that will perform the following actions:
+- ask the user for input
+- transform all letters in the input into lowercase
+- print the resulting lowercase string
+"""
 
+sentence = input(f"Write sentence please: ")
+
+low_sentence = sentence.lower()
+
+print(low_sentence)
