5a9ce69ff5d6d55a72769a191e4cf6953329a099
Ivo Marek <ivo172@gmail.com>
2020-01-22 17:03:38 +0100

    OK

--- Slicing.py
+++ Slicing.py
@@ -5,19 +5,20 @@ last 5 letters in word 'indexing',
 first letter in word 'indexing', then stepping by 3.
 Example of running program:
 First five letters: index
-Last fice letters: exing
+Last five letters: exing
 Every third letter: ien
 Save each slice in a variable and then print variable in sentence (for example 'First five letters: ' + variable).
 """
 
-word_indexing = "indexing"
+word_indexing = "indexing"      # string
 
 # Extract and print first 5 letters
-print()
-
+first_five = (word_indexing[0:5])
 
 # Extract and print last 5 letters
-
-
+last_five = (word_indexing[-5:-1])
 
 # Extract and print each 3rd letter
+every_third = (word_indexing[::3])
+
+print(f"First five letters are ´{first_five}´, last five letters are ´{last_five}´ and every third letter is ´{every_third}´.")
