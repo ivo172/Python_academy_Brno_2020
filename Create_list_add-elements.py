4bd032badfa4541345c0ae30750d54f3af83c64b
Ivo Marek <ivo172@gmail.com>
2020-01-22 21:26:19 +0100

    OK

--- Create_list_add-elements.py
+++ Create_list_add-elements.py
@@ -1,22 +1,34 @@
 # Create Candidate
 
+beginning = ["Francis", "Ann", "Jacob", "Claire"]
 
 # Print candidates at the beginning
 
+print(f"Employess at the beginning {beginning}")
 
 # Create employees
+
+employees = ["Bruno", "Agnes"]
 
 
 # Print employees at the beginning
 
+print(f"Candidates are {employees}")
 
 # Add new candidates
 
+beginning = beginning + employees
 
 # Print new candidates
 
+print(f"New candidates are {beginning}")
 
 # Insert name
 
+new_name = input("Insert name pls.: ")
+new_name = (new_name)
+
+beginning.append(new_name)
 
 # Print the employees list after entering a new name
+print(f"All employees: {beginning}")
