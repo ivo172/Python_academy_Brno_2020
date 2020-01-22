86cffd2fb117d850bdeb478abf13cd9b6ed26867
Ivo Marek <ivo172@gmail.com>
2020-01-22 18:54:28 +0100

    OK

--- String_operations.py
+++ String_operations.py
@@ -1,1 +1,32 @@
+# Save name
 
+name = input("Name: ")
+
+# Print name
+
+print(f"Your name is: {name}")
+
+# Save surname
+
+surname = input("Surname: ")
+
+# Print surname
+
+print(f"Your surname is: {surname}")
+
+# Create and print variable full_name
+
+print(f"Your full name is {name} {surname}.")
+
+
+# Create and print variable name_length
+
+fullname = name + " " + surname
+name_length = len(fullname)
+print(f"Your full name has {name_length - 1} letters.")
+
+# Print bounded variable full_name
+
+print("=" * name_length)
+print(fullname)
+print("=" * name_length)
