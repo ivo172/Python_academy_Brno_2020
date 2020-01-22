d462e297446f665f7c11ea18fd3829143d902479
Ivo Marek <52607717+ivo172@users.noreply.github.com>
2020-01-22 12:04:44 +0100

    Create Unit_converter.py

---
+++ Unit_converter.py
@@ -0,0 +1,26 @@
+# Conversion rates
+# kg_lb = 2.20
+# km_mile = 0.62
+# l_gal = 0.26
+
+# Amount of units for conversion.
+weight_kg = 80
+distance_km = 54
+capacity_l = 5
+
+# Conversion calculations
+
+weight_lb = weight_kg * 2.20
+distance_mile = distance_km * 0.62
+capacity_gal = capacity_l * 0.26
+
+# Final answers
+
+print(f"Conversion result: weight {weight_kg} kg is {weight_lb} lb, distance {distance_km} km is {distance_mile} mile "
+      f"and capacity {capacity_l} l is {capacity_gal} gal.")
+
+# Your task is to convert:
+# 80 kg to lb
+# 54 km to miles
+# 5 l to gal
+# Finally, print results in sentences
