2f5b17c865be6adfca74137059202cc3be765c5a
Ivo Marek <ivo172@gmail.com>
2020-01-22 13:28:05 +0100

    OK

---
+++ Slicing.py
@@ -0,0 +1,22 @@
+# Create a program, that will use slicing to print and extract specific parts of word "indexing"
+Create a program, that will use the indexing to print and extract:
+
+first 5 letters in word 'indexing',
+last 5 letters in word 'indexing',
+first letter in word 'indexing', then stepping by 3.
+Example of running program:
+
+First five letters: index
+Last fice letters: exing
+Every third letter: ien
+Save each slice in a variable and then print variable in sentence (for example 'First five letters: ' + variable).
+
+# Extract and print first 5 letters
+
+
+
+# Extract and print last 5 letters
+
+
+
+# Extract and print each 3rd letter
