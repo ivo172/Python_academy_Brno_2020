80029c38d82c57664e7c20d0e7f737c1e6a65b08
Ivo Marek <ivo172@gmail.com>
2020-01-23 18:27:46 +0100

    OK

--- List_operations_2.py
+++ List_operations_2.py
@@ -4,14 +4,23 @@ employees = ['Francis', 'Bruno', 'Ann', 'Jacob', 'Claire', 'Agnes', 'Agnes', 'Ag
 
 # Interval 2-5
 
+interval2_5 = employees[2:6]
+print(f"From index 2 to 5 there are: {interval2_5}")
 
 # Each 3rd
 
+each3rd = employees[::3]
+print(f"Every third member is: {each3rd}")
 
 # Save index
 
+jacob_index = employees.index("Jacob")
 
 # Jacob index
 
+print(f"Jacob is on the index: {jacob_index}")
 
 # Number of name Agnes
+
+agnes_count = employees.count("Agnes")
+print(f"Number of repetitions of the name Agnes: {agnes_count}")
