4ea09deec957e82635915caf8579ef7c8058f60a
Ivo Marek <ivo172@gmail.com>
2020-01-23 15:43:09 +0100

    OK

---
+++ List_operations_2.py
@@ -0,0 +1,17 @@
+# Results from the previous task
+candidates = ['Agnes', 'Agnes', 'Agnes']
+employees = ['Francis', 'Bruno', 'Ann', 'Jacob', 'Claire', 'Agnes', 'Agnes', 'Agnes']
+
+# Interval 2-5
+
+
+# Each 3rd
+
+
+# Save index
+
+
+# Jacob index
+
+
+# Number of name Agnes
