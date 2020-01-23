473c49adf93bca22056d113b29312acfc23d57b3
Ivo Marek <ivo172@gmail.com>
2020-01-23 11:04:17 +0100

    OK

---
+++ List_operations_1.py
@@ -0,0 +1,31 @@
+# Results from previous task
+candidates = ['Bruno', 'Agnes']
+employees = ['Francis', 'Bruno', 'Ann', 'Jacob', 'Claire']
+
+
+
+# Delete names from candidates
+
+
+# Print remaining candidates
+
+
+# Repeat element
+
+
+# Print repeating element in list candidates
+
+
+# Join lists
+
+
+# Print joined lists
+
+
+# Index 2
+
+
+# Find out last index and assign it to variable
+
+
+# Last index
