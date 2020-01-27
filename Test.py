edea57602b579345dcaa6c76c75729af5d2484ad
Ivo Marek <ivo172@gmail.com>
2020-01-27 20:23:27 +0100

    simplif... if

---
+++ Test.py
@@ -0,0 +1,9 @@
+my_str = 'Python'
+result = ''
+
+#if my_str.istitle():
+#    result = 'Titlecased'
+#else:
+#    result = 'Not titlecased'
+
+result = "Titlecased" if my_str == "Titlecased" else "Not titlecased"
