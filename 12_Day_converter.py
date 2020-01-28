080cc976c07eb6631d2e0f240c4d106c94df4e87
Ivo Marek <ivo172@gmail.com>
2020-01-28 23:24:15 +0100

    All ok X# Please enter the commit message for your changes. Lines starting

---
+++ 12_Day_converter.py
@@ -0,0 +1,5 @@
+
+# Your task is to create a script called convert_day.py that will: ask for a number between 1 to 7 return the name of
+# corresponding weekday (1 - 'Monday', 2-'Tuesday', 3 - 'Wednesday', 4 - 'Thursday', 5 - 'Friday', 6 - 'Saturday',
+# 7 - 'Sunday') if no input has been provided (user hitting enter without typing anything), the program should print:
+# 'No input provided' if the input is not a number the program should print: 'Enter only numbers, please'
