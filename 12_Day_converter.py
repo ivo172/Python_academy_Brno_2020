c6bc3306b6906d9bbd92b6b6de0feacbe22bb285
Ivo Marek <ivo172@gmail.com>
2020-01-29 19:39:26 +0100

    Added homework No.12 convert_day

--- 12_Day_converter.py
+++ 12_Day_converter.py
@@ -1,7 +1,19 @@
-
 # Your task is to create a script called convert_day.py that will: ask for a number between 1 to 7 return the name of
 # corresponding weekday (1 - 'Monday', 2-'Tuesday', 3 - 'Wednesday', 4 - 'Thursday', 5 - 'Friday', 6 - 'Saturday',
 # 7 - 'Sunday') if no input has been provided (user hitting enter without typing anything), the program should print:
 # 'No input provided' if the input is not a number the program should print: 'Enter only numbers, please'
-import datetime
+
+weekday = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+selected_number = input("Please enter number of the day: ")
+
+if selected_number.isalpha():
+    print("Enter only number, please.")
+elif int(selected_number) < 1 or int(selected_number) > 7:
+        print("Number in range 1 to 7, please!")
+else:
+    selected_day = weekday[int(selected_number) - 1]
+    print(selected_day)
+
+
 
