6cda041c2144557a2d92830caa06957bd1341b25
Ivo Marek <ivo172@gmail.com>
2020-02-05 18:59:30 +0100

    after merge with develop...

--- Project_1.py
+++ Project_1.py
@@ -79,9 +79,10 @@ other freshwater genera and herring similar to those
 in modern oceans. Other fish such as paddlefish, 
 garpike and stingray are also present.'''
 ]
-users = {'bob':123, 'ann':'pass123', 'mike':'password123', 'liz':123}
 
-print(80 * '-') # Print line
+USERS = {'bob':123, 'ann':'pass123', 'mike':'password123', 'liz':123}
+
+print(80*'-')
 print('Welcome to the app. Please log in:')
 username = input('USER: ')
 
@@ -104,13 +105,8 @@ choice_text = input('We have 3 texts to be analyzed. '
                     'Enter a number btw. 1 and 3 to select: ')
 print(80 * '-') # Print line
 
-text = TEXTS[(int(choice_text)) - 1]    # =>int
-                                        # 1, 2 and 3 is in list, therefore -1
-print(f'Your selection is this text: \n{text}')
-print(80 * '-') # Print line
 
-
-
+print(f'For key {username} is value {USERS[username]}')
 
 
 
