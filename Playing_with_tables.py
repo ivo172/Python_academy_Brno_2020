d6dc39e01b89b9a925683053e60ac6a1b90485d7
Ivo Marek <ivo172@gmail.com>
2020-02-20 17:03:10 +0100

    Added Playing_with_tables.

---
+++ Playing_with_tables.py
@@ -0,0 +1,11 @@
+# Vytvořte program, který uživateli umožní provádět následující akce:
+# 1. Vytvořte tabulku
+# 2. Vložte nový řádek do dané tabulky
+# 3. Vložte nový sloupec do dané tabulky
+# 4. Aktualizujte buňku v tabulce
+# 5. Vypočítat součet pro vybraný sloupec
+# 6. Vypočítat součet pro vybraný řádek
+# 7. Tisk celé tabulky nebo vybraných řádků
+# 8. Export tabulky do slovníku
+# Program by měl běžet ve smyčce, dokud mu uživatel neřekne ukončení. 
+
