be29b8ccaa513a55f12832e601cc922917771fba
Ivo Marek <ivo172@gmail.com>
2020-02-20 08:28:17 +0100

    add HW 28

---
+++ 28_Sum_powers.py
@@ -0,0 +1,10 @@
+'''
+Write a Python script that will ask the user to enter a number from which it will 
+compute the result. The result should be the sum of numbers less than or equal 
+to the input number, each raised to power of its value. 
+Then the script should print the result to the terminal.
+
+For example:
+- if the user enters number 5, the program should compute the sum as: 1**1 + 2**2 + 3**3 + 4**4 + 5**5.
+- if the user enters 6, then it should be: 1**1 + 2**2 + 3**3 + 4**4 + 5**5 + 6**6
+...and so on. '''
