38750595566e00eb112a37907b429e182ff2ea91
ivo172 <ivo172@gmail.com>
2020-02-27 21:59:02 +0100

    add 38. Min & Max

---
+++ 38_Min&Max.py
@@ -0,0 +1,20 @@
+def my_min(numbers):
+    min_number = numbers.pop(0)
+    for number in numbers:
+        if number <= min_number:
+            min_number = number
+        else:
+            continue
+    return min_number
+
+def my_max(numbers):
+    max_number = numbers.pop(0)
+    for number in numbers:
+        if number >= max_number:
+            max_number = number
+        else:
+            continue
+    return max_number
+
+print(my_min([8,16,56,41,2,55]))
+print(my_max([8,16,56,41,2,55]))
