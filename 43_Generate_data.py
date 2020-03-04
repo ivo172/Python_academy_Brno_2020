4bb3eb5ad3ff49e0aaae8ce4791c4d59d6aefd4a
Ivo Marek <ivo172@gmail.com>
2020-03-04 21:07:09 +0100

    ok

---
+++ 43_Generate_data.py
@@ -0,0 +1,14 @@
+customers = ['Bettison, Elnora',
+             'Doro, Jeffrey',
+             'Idalia, Craig',
+             'Conyard, Phil',
+             'Skupinski, Wilbert',
+             'McShee, Glenn',
+             'Pate, Ashley',
+             'Woodison, Annie']
+
+products = [('DROXIA', 33.86),('WRINKLESS PLUS',23.55),
+            ('Claravis', 9.85), ('Nadolol', 12.35),
+            ('Quinapril', 34.89), ('Doxycycline Hyclate', 23.43),
+            ('Metolazone', 43.06), ('PAXIL', 14.78)]
+
--- New_request.py
+++ New_request.py
@@ -10,10 +10,10 @@
 # print(json_result)
 
 import numpy as np
-import matpolotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 def func(x):
-    y = 2 * x + 2
+    y = 2**x + 2
     return y
 
 x = np.arange(0, 10)
