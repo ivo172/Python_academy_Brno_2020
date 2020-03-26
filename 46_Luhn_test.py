8cf88125b40101b5ee84b21bb348dcfc05626d5f
ivo172 <ivo172@gmail.com>
2020-03-26 23:22:11 +0100

    Merge branch 'master' of https://github.com/ivo172/Python_academy_Brno_2020
    # Conflicts:
    #	46_Luhn_test.py
    #	File_handler.py
    #	text.txt

--- 46_Luhn_test.py
+++ 46_Luhn_test.py
@@ -1,6 +1,5 @@
 def luhn(num):
     num_str = str(num).replace(' ', '')
-
     s1 = s2 = 0
 
     for i, num in enumerate(num_str[::-1]):
@@ -12,5 +11,7 @@ def luhn(num):
                 num = int(str(num)[0]) + int(str(num)[1])
             s2 += int(num)
     result = True if (s1 + s2) % 10 == 0 else False
-
     return result
+
+
+print(luhn(49927398716))
