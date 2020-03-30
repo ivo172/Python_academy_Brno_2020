542e66ac66b0e980b1e7c3a17fda5bb289c53a4c
Ivo Marek <ivo172@gmail.com>
2020-03-30 22:53:30 +0200

    Added 74

--- 74_Fill_in_contracts.py
+++ 74_Fill_in_contracts.py
@@ -11,14 +11,14 @@
 # and save it under employees.txt. You should remember the place where you store it, 
 # because you will need to open it in your program.
 
-{'X12345' : {'ID': 'X12345', 'full_name': 'Jack Frank',
-            'birthdate': '1.1.1970' , 'job_title' : 'welder',
-            'position_from':'1.5.2015', 'contract_start':'1.2.2013',
-            'contract_end':'31.12.2020', 'salary': 123456},
-'X54321' : {'ID': 'X54321', 'full_name': 'Bob Doe',
-            'birthdate': '8.8.1971' , 'job_title' : 'machinist',
-            'position_from':'1.8.2016', 'contract_start':'1.8.2014',
-            'contract_end':'31.12.2021', 'salary': 23451}}
+# {'X12345': {'ID': 'X12345', 'full_name': 'Jack Frank',
+#                    'birthdate': '1.1.1970', 'job_title': 'welder',
+#                    'position_from': '1.5.2015', 'contract_start': '1.2.2013',
+#                    'contract_end': '31.12.2020', 'salary': 123456},
+#         'X54321': {'ID': 'X54321', 'full_name': 'Bob Doe',
+#                    'birthdate': '8.8.1971', 'job_title': 'machinist',
+#                    'position_from': '1.8.2016', 'contract_start': '1.8.2014',
+#                    'contract_end': '31.12.2021', 'salary': 23451}}
 
 # Contract templates:
 # Please store the following text templates under the corresponding names. 
@@ -28,4 +28,23 @@
 # - job_change.txt
 # - contract_prolongation.txt
 
-            
+import os.path
+path = os.path.dirname(__file__)
+
+
+def add_job_change(read_text, full_name, id, birthdate, job_title, position_from):
+    read_text = read_text.replace('{full_name}', full_name)
+    read_text = read_text.replace('{ID}', id)
+    read_text = read_text.replace('{birthdate}', birthdate)
+    read_text = read_text.replace('{job_title}', job_title)
+    read_text = read_text.replace('{position_from}', position_from)
+    return read_text
+
+
+index_file = int(input('Enter: '))
+
+if index_file == 1:
+    with open(path + '\job_change.txt') as text:
+        read_text = text.read()
+        result = add_job_change(read_text, 'Ivo Marek', '123458', '27.4.1965', 'mechanik', '27.1.1965')
+        print(result)
