694e7ae5a11bbf4b19c32dbd131c6423c21c795b
ivo172 <ivo172@gmail.com>
2020-04-04 21:58:23 +0200

    Add 75

---
+++ 75_List_Divisors.py
@@ -0,0 +1,5 @@
+# We would like to find out, what numbers in a given range are divisible by each of the numbers btw. 2 and 9.
+# Your task is to create a program that will:
+# - collect the information about numbers divisible by each single digit number between 2 and 9
+# - print the information in nicely formatted table
+
