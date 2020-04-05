4d033b1590bd13cfc6b426f2e9f6f9b4fbd25025
ivo172 <ivo172@gmail.com>
2020-04-05 18:52:59 +0200

    75 ...

--- 75_List_Divisors.py
+++ 75_List_Divisors.py
@@ -3,3 +3,24 @@
 # - collect the information about numbers divisible by each single digit number between 2 and 9
 # - print the information in nicely formatted table
 
+
+start_point = int(input('Start point: '))
+end_point = (int(input('End point: '))) + 1
+
+i = 2
+
+
+def string_x(start_point, end_point, i):  # i = divisor
+    num_ = ''
+    for num in range(start_point, end_point):
+        if num % i == 0:
+            num_ = num_ + ' ' + str(num)
+    return num_
+
+
+print('| Divisor |    Number Divided     |')        # Formatovani neresim ...
+print('===================================')
+
+for i in range(2, 10):
+    result = string_x(start_point, end_point, i)
+    print(f'|    {i}    | {result} |')
