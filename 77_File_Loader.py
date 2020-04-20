6e941c268b872bea584eb6fff74560f1eeb42d0f
ivo172 <ivo172@gmail.com>
2020-04-20 20:15:15 +0200

    add 77...

--- 77_File_Loader.py
+++ 77_File_Loader.py
@@ -1,1 +1,17 @@
-def file_loader(path):
+def file_loader(filepath):
+
+    try:
+        file = open(filepath)
+
+    except FileNotFoundError:
+        filename = filepath.split('/')[-1]
+        print('File {} does not exist.'.format(filename))
+
+    else:
+        content = file.read()
+        file.close()
+        return content
+
+
+filepath = input('Enter filepath: ')
+print(file_loader(filepath))
